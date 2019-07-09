724a53616b5ca0c0b46608b318c6d02f000ccb99
Matteo Pergolesi <matpergo@gmail.com>
2019-07-09 12:54:43 +0200

    Simple long loop to test out the debugger.

--- src/main/java/it/cnit/nfvmano/App.java
+++ src/main/java/it/cnit/nfvmano/App.java
@@ -4,10 +4,14 @@ package it.cnit.nfvmano;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws InterruptedException {
+        int i = 0;
+        while (i<100) {
+            System.out.println("Hello World!");
+            Thread.sleep(1000);
+            i++;
+        }
     }
 }
