f608580dce0840cb0549bca220ab9f0851c1fae4
Matteo Pergolesi <matpergo@gmail.com>
2019-07-22 17:23:10 +0200

    Add google Java code style. Reformatting class.

--- src/main/java/it/cnit/nfvmano/App.java
+++ src/main/java/it/cnit/nfvmano/App.java
@@ -2,16 +2,15 @@ package it.cnit.nfvmano;
 
 /**
  * Hello world!
- *
  */
-public class App
-{
-    public static void main( String[] args ) throws InterruptedException {
-        int i = 0;
-        while (i<100) {
-            System.out.println("Hello World!");
-            Thread.sleep(1000);
-            i++;
-        }
+public class App {
+
+  public static void main(String[] args) throws InterruptedException {
+    int i = 0;
+    while (i < 100) {
+      System.out.println("Hello World!");
+      Thread.sleep(1000);
+      i++;
     }
+  }
 }
