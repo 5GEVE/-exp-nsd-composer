d9beb4dc7b65f057dad38253b81ef0eda9fdb4af
Matteo Pergolesi <matpergo@gmail.com>
2019-07-24 11:35:45 +0200

    Including a ifa nsd stub file. Deserializing and deserializing.

--- src/main/java/it/cnit/nfvmano/App.java
+++ src/main/java/it/cnit/nfvmano/App.java
@@ -1,16 +1,23 @@
 package it.cnit.nfvmano;
 
-/**
- * Hello world!
- */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import it.nextworks.nfvmano.libs.descriptors.nsd.Nsd;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
 
-  public static void main(String[] args) throws InterruptedException {
-    int i = 0;
-    while (i < 100) {
-      System.out.println("Hello World!");
-      Thread.sleep(1000);
-      i++;
-    }
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper(new YAMLFactory());
+  private static Logger LOG = LoggerFactory
+      .getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
+
+  public static void main(String[] args) throws IOException {
+    InputStream is = App.class.getResourceAsStream("/nsd_stub.yaml");
+    Nsd nsd_stub = OBJECT_MAPPER.readValue(is, Nsd.class);
+    LOG.info("Dump:\n{}", OBJECT_MAPPER.writeValueAsString(nsd_stub));
   }
 }
