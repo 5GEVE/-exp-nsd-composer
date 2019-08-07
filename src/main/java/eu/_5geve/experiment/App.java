3044c5323f706a39afd9bb83abb63622ab76d557
Matteo Pergolesi <matpergo@gmail.com>
2019-08-07 15:59:17 +0200

    Add jgrapht-ext dependency. Simple program to visualize a graph.

--- src/main/java/eu/_5geve/experiment/App.java
+++ src/main/java/eu/_5geve/experiment/App.java
@@ -7,6 +7,13 @@ import it.nextworks.nfvmano.libs.descriptors.nsd.Nsd;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
+import javax.swing.JFrame;
+import org.jgrapht.Graph;
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultListenableGraph;
+import org.jgrapht.graph.DefaultUndirectedGraph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,5 +32,39 @@ public class App {
     VsBlueprint vsb = OBJECT_MAPPER.readValue(isVsb, VsBlueprint.class);
     LOG.info("Dump:\n{}", OBJECT_MAPPER.writeValueAsString(vsb));
 
+    createNSDGraph();
+
+  }
+
+  private static void createNSDGraph() throws IOException {
+
+    // create a JGraphT graph
+    ListenableGraph<String, DefaultEdge> g = new DefaultListenableGraph<>(
+        new DefaultUndirectedGraph<>(DefaultEdge.class));
+    String v1 = "v1";
+    String v2 = "v2";
+    String v3 = "v3";
+    String v4 = "v4";
+
+    // add some sample data (graph manipulated via JGraphX)
+    g.addVertex(v1);
+    g.addVertex(v2);
+    g.addVertex(v3);
+    g.addVertex(v4);
+
+    g.addEdge(v1, v2);
+    g.addEdge(v2, v3);
+    g.addEdge(v3, v1);
+    g.addEdge(v4, v3);
+
+    GraphJApplet applet = new GraphJApplet();
+    applet.init(g);
+
+    JFrame frame = new JFrame();
+    frame.getContentPane().add(applet);
+    frame.setTitle("JGraphT Adapter to JGraphX Demo");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
   }
 }
