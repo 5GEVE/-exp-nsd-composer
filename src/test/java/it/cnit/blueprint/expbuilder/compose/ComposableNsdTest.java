cbf085d710db676f1c1d2e1b4187dee7a1b99be0
Matteo Pergolesi <matpergo@gmail.com>
2020-02-03 12:21:40 +0100

    Add logging profile for tests.

--- src/test/java/it/cnit/blueprint/expbuilder/compose/ComposableNsdTest.java
+++ src/test/java/it/cnit/blueprint/expbuilder/compose/ComposableNsdTest.java
@@ -21,10 +21,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("logging-test")
 public class ComposableNsdTest {
 
   final static Logger LOG = LoggerFactory.getLogger(ComposableNsdTest.class);
