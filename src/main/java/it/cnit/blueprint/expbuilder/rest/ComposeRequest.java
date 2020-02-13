7e7914d1b26674288f49040a0119942b4744dd44
Matteo Pergolesi <matpergo@gmail.com>
2020-02-13 18:00:20 +0100

    Optimize imports.

--- src/main/java/it/cnit/blueprint/expbuilder/rest/ComposeRequest.java
+++ src/main/java/it/cnit/blueprint/expbuilder/rest/ComposeRequest.java
@@ -1,7 +1,6 @@
 package it.cnit.blueprint.expbuilder.rest;
 
 import it.nextworks.nfvmano.catalogue.blueprint.messages.OnBoardVsBlueprintRequest;
-import java.util.List;
 import lombok.Data;
 
 @Data
