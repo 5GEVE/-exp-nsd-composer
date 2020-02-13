102a6b054c332d5ed3166f2b26f1d8d6b1e68688
Matteo Pergolesi <matpergo@gmail.com>
2020-02-13 15:35:25 +0100

    Use Lombok for CtxComposeResource.

--- src/main/java/it/cnit/blueprint/expbuilder/rest/CtxComposeResource.java
+++ src/main/java/it/cnit/blueprint/expbuilder/rest/CtxComposeResource.java
@@ -3,7 +3,9 @@ package it.cnit.blueprint.expbuilder.rest;
 import it.cnit.blueprint.expbuilder.compose.ComposableNsd.CompositionStrat;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.Nsd;
 import java.util.Map;
+import lombok.Data;
 
+@Data
 public class CtxComposeResource {
 
   private Nsd nsd;
@@ -12,35 +14,4 @@ public class CtxComposeResource {
   // Map<vnfProfileId, vlProfileId>
   private Map<String, String> VirtualLinkIds;
 
-  public Nsd getNsd() {
-    return nsd;
-  }
-
-  public void setNsd(Nsd nsd) {
-    this.nsd = nsd;
-  }
-
-  public CompositionStrat getStrat() {
-    return strat;
-  }
-
-  public void setStrat(CompositionStrat strat) {
-    this.strat = strat;
-  }
-
-  public String getSapId() {
-    return SapId;
-  }
-
-  public void setSapId(String sapId) {
-    SapId = sapId;
-  }
-
-  public Map<String, String> getVirtualLinkIds() {
-    return VirtualLinkIds;
-  }
-
-  public void setVirtualLinkIds(Map<String, String> virtualLinkIds) {
-    VirtualLinkIds = virtualLinkIds;
-  }
 }
