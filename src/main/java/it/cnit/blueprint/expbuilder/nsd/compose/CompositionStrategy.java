dd20aed244dd3420bd6a66d0040326fed37b22c4
Matteo Pergolesi <matpergo@gmail.com>
2020-02-18 19:12:49 +0100

    Refactor method.

--- src/main/java/it/cnit/blueprint/expbuilder/nsd/compose/CompositionStrategy.java
+++ src/main/java/it/cnit/blueprint/expbuilder/nsd/compose/CompositionStrategy.java
@@ -5,10 +5,24 @@ import it.cnit.blueprint.expbuilder.rest.InvalidCtxComposeInfo;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsDf;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsLevel;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.Nsd;
+import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.VnfProfile;
+import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.VnfToLevelMapping;
 
-public interface CompositionStrategy {
+public abstract class CompositionStrategy {
 
-  void compose(Nsd nsd, NsDf nsDf, NsLevel nsLevel, CtxComposeInfo composeInfo)
+  void addVnf(Nsd nsd, NsDf nsDf, NsLevel nsLevel, VnfProfile ctxVnfProfile,
+      VnfToLevelMapping vnfLvlMap) {
+    if (nsd.getVnfdId().stream().noneMatch(id -> id.equals(ctxVnfProfile.getVnfdId()))) {
+      nsd.getVnfdId().add(ctxVnfProfile.getVnfdId());
+    }
+    if (nsDf.getVnfProfile().stream()
+        .noneMatch(vp -> vp.getVnfProfileId().equals(ctxVnfProfile.getVnfProfileId()))) {
+      nsDf.getVnfProfile().add(ctxVnfProfile);
+    }
+    nsLevel.getVnfToLevelMapping().add(vnfLvlMap);
+  }
+
+  abstract void compose(Nsd nsd, NsDf nsDf, NsLevel nsLevel, CtxComposeInfo composeInfo)
       throws InvalidCtxComposeInfo;
 
 }
