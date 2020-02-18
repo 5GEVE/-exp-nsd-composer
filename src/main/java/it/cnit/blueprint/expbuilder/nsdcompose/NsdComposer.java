26aa4266cd2482f3d3572981c1ac759e4e6033e8
Matteo Pergolesi <matpergo@gmail.com>
2020-02-18 17:04:29 +0100

    Add nsd variable dump.

--- src/main/java/it/cnit/blueprint/expbuilder/nsdcompose/NsdComposer.java
+++ src/main/java/it/cnit/blueprint/expbuilder/nsdcompose/NsdComposer.java
@@ -1,5 +1,8 @@
 package it.cnit.blueprint.expbuilder.nsdcompose;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import it.cnit.blueprint.expbuilder.nsdcompose.CompositionStrategy;
 import it.cnit.blueprint.expbuilder.nsdgraph.NsdGraphService;
 import it.cnit.blueprint.expbuilder.nsdgraph.ProfileVertex;
@@ -10,6 +13,7 @@ import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsDf;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsLevel;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.Nsd;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.NotImplementedException;
 import org.jgrapht.Graph;
@@ -38,11 +42,14 @@ public class NsdComposer {
     PASSTHROUGH
   }
 
+  @SneakyThrows(JsonProcessingException.class)
   public void composeWith(Nsd nsd, CtxComposeInfo[] ctxRArray)
       throws InvalidCtxComposeInfo, InvalidNsd {
+    ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
     for (CtxComposeInfo ctxR : ctxRArray) {
       for (NsDf df : nsd.getNsDf()) {
         for (NsLevel l : df.getNsInstantiationLevel()) {
+          log.debug("Nsd before:\n{}", objectMapper.writeValueAsString(nsd));
           Graph<ProfileVertex, String> g = nsdGraphService.buildGraph(nsd.getSapd(), df, l);
           log.debug("Graph export before:\n{}", nsdGraphService.export(g));
           if (ctxR.getStrat() == CompositionStrat.CONNECT) {
@@ -53,6 +60,7 @@ public class NsdComposer {
             throw new NotImplementedException(
                 String.format("Composition strategy %s not implemented", ctxR.getStrat()));
           }
+          log.debug("Nsd after:\n{}", objectMapper.writeValueAsString(nsd));
           g = nsdGraphService.buildGraph(nsd.getSapd(), df, l);
           log.debug("Graph export after:\n{}", nsdGraphService.export(g));
         }
