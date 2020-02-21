d1786cc9895d900ec95d974b891060f2a7441008
Matteo Pergolesi <matpergo@gmail.com>
2020-02-21 09:14:21 +0100

    Use @AllArgsConstructor on VnfWrapper.

--- src/main/java/it/cnit/blueprint/expbuilder/nsd/compose/VnfWrapper.java
+++ src/main/java/it/cnit/blueprint/expbuilder/nsd/compose/VnfWrapper.java
@@ -3,18 +3,15 @@ package it.cnit.blueprint.expbuilder.nsd.compose;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsVirtualLinkConnectivity;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.VnfProfile;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.VnfToLevelMapping;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
+@AllArgsConstructor
 public class VnfWrapper {
 
-  @NonNull
   private VnfToLevelMapping vnfToLevelMapping;
-  @NonNull
   private VnfProfile vnfProfile;
-  @NonNull
   private NsVirtualLinkConnectivity vlConnectivity;
-  @NonNull
   private String vfndId;
 }
