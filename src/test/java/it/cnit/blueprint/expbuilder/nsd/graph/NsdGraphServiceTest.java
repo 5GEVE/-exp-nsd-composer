d089185c3b62011a856eeaf090cde6f8ce8d6f14
Matteo Pergolesi <matpergo@gmail.com>
2020-02-24 12:59:00 +0100

    Add tests for graph building and export.

---
+++ src/test/java/it/cnit/blueprint/expbuilder/nsd/graph/NsdGraphServiceTest.java
@@ -0,0 +1,80 @@
+package it.cnit.blueprint.expbuilder.nsd.graph;
+
+import static org.junit.Assert.assertEquals;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.Nsd;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+import java.util.Scanner;
+import lombok.SneakyThrows;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NsdGraphServiceTest {
+
+  static Properties prop;
+  static ObjectMapper oM;
+  static NsdGraphService nsdGraphService;
+
+  @BeforeClass
+  @SneakyThrows
+  public static void setUp() {
+    // Test Setup
+    prop = new Properties();
+    InputStream input = ClassLoader.getSystemResourceAsStream("url.properties");
+    prop.load(input);
+    oM = new ObjectMapper(new YAMLFactory());
+    nsdGraphService = new NsdGraphService(new GraphVizExporter());
+  }
+
+  @Test
+  @SneakyThrows
+  public void buildGraphAres2TDelayExperimentBig() {
+    // Given
+    Nsd nsd = oM.readValue(new URL(prop.getProperty("expb.tracker.delay.nsds")), Nsd[].class)[0];
+    String nsLevel = "ns_ares2t_tracker_exp_il_big";
+
+    // When
+    String actual = nsdGraphService.export(nsdGraphService
+        .buildGraph(nsd.getSapd(), nsd.getNsDf().get(0), nsd.getNsDf().get(0).getNsLevel(nsLevel)));
+
+    // Then
+    InputStream in = ClassLoader.getSystemResourceAsStream(nsLevel + ".dot");
+    String expected;
+    //noinspection ConstantConditions
+    try (Scanner scanner = new Scanner(ClassLoader.getSystemResourceAsStream(nsLevel + ".dot"),
+        StandardCharsets.UTF_8.name())) {
+      expected = scanner.useDelimiter("\\A").next();
+    }
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @SneakyThrows
+  public void buildGraphAres2TDelayExperimentSmall() {
+    // Given
+    Nsd nsd = oM.readValue(new URL(prop.getProperty("expb.tracker.delay.nsds")), Nsd[].class)[0];
+    String nsLevel = "ns_ares2t_tracker_exp_il_small";
+
+    // When
+    String actual = nsdGraphService.export(nsdGraphService
+        .buildGraph(nsd.getSapd(), nsd.getNsDf().get(0), nsd.getNsDf().get(0).getNsLevel(nsLevel)));
+
+    // Then
+    InputStream in = ClassLoader.getSystemResourceAsStream(nsLevel + ".dot");
+    String expected;
+    //noinspection ConstantConditions
+    try (Scanner scanner = new Scanner(ClassLoader.getSystemResourceAsStream(nsLevel + ".dot"),
+        StandardCharsets.UTF_8.name())) {
+      expected = scanner.useDelimiter("\\A").next();
+    }
+
+    assertEquals(expected, actual);
+  }
+
+}
