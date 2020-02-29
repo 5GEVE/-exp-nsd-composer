2a00cab6bc776a1a7b3ae16ab340013a02f2bbcb
Matteo Pergolesi <matpergo@gmail.com>
2020-02-29 19:55:05 +0100

    Rename class to ExperimentsController.

--- src/main/java/it/cnit/blueprint/expbuilder/rest/ExperimentsController.java
+++ src/main/java/it/cnit/blueprint/expbuilder/rest/ExperimentsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-public class Controller {
+public class ExperimentsController {
 
   private final NsdComposer nsdComposer;
 
