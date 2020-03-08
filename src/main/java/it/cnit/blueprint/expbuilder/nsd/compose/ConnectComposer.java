c70703477ff2eb025767995d74d8df41d26b88fd
Matteo Pergolesi <matpergo@gmail.com>
2020-03-08 17:57:13 +0100

    Retrieve src and dst VLs.

--- src/main/java/it/cnit/blueprint/expbuilder/nsd/compose/ConnectComposer.java
+++ src/main/java/it/cnit/blueprint/expbuilder/nsd/compose/ConnectComposer.java
@@ -7,6 +7,8 @@ import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsDf;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsLevel;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsVirtualLinkDesc;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.Nsd;
+import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.jgrapht.Graph;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -34,6 +36,7 @@ public class ConnectComposer extends NsdComposer {
     // Retrieve ctx VNFs
     VnfInfo srcVnfInfo;
     VnfInfo dstVnfInfo;
+    // Assumption: src is 0 and dst is 1
     try {
       String srcVnfdId = ctxNsd.getVnfdId().get(0);
       srcVnfInfo = retrieveVnfInfoByDescId(srcVnfdId, ctxNsd, ctxNsDf, ctxNsLvl);
@@ -47,6 +50,40 @@ public class ConnectComposer extends NsdComposer {
     log.debug("Added Vnfd='{}' in service (if not present).", srcVnfInfo.getVfndId());
     addVnf(dstVnfInfo, vsbNsd, vsbNsDf, vsbNsLvl);
     log.debug("Added Vnfd='{}' in service (if not present).", dstVnfInfo.getVfndId());
+
+    // Retrieve src VL
+    // Retrieve RAN VL information from vsb
+    VlInfo srcVlInfo;
+    try {
+      srcVlInfo = retrieveVlInfo(ranVld, vsbNsDf, vsbNsLvl);
+      log.debug("Found VlInfo for ranVld='{}' in vsbNsd.", ranVld.getVirtualLinkDescId());
+    } catch (InvalidNsd | VlNotFoundInLvlMapping e) {
+      log.error(e.getMessage());
+      throw new InvalidNsd(e.getMessage());
+    }
+    // Retrieve dst VL
+    VlInfo dstVlInfo;
+    Optional<NsVirtualLinkDesc> optdstVld = vsbNsd.getVirtualLinkDesc().stream()
+        .filter(vld -> vld.getVirtualLinkDescId().equals(vsbMgmtVld.getVirtualLinkDescId()))
+        .findFirst();
+    try {
+      if (optdstVld.isPresent()) {
+        dstVlInfo = retrieveVlInfo(optdstVld.get(), vsbNsDf, vsbNsLvl);
+        log.debug("Found non-mgmt VlInfo.");
+      } else {
+        throw new InvalidNsd(
+            "Can't find a non-mgmt VlInfo in vsbNsd: '" + vsbNsd.getNsdIdentifier() + "'");
+      }
+    } catch (InvalidNsd | VlNotFoundInLvlMapping e) {
+      log.error(e.getMessage());
+      throw new InvalidNsd(e.getMessage());
+    }
+
+    // Connect src VNF
+    connectVnfToVL(srcVnfInfo.getVnfProfile(), "???", srcVlInfo.getVlProfile());
+
+    // Connect dst VNF
+    connectVnfToVL(dstVnfInfo.getVnfProfile(), "???", dstVlInfo.getVlProfile());
 
   }
 }
