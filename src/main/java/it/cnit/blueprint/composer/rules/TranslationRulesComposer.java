91ed8da532dca8fc651fa1f721a62b58aad733d4
Matteo Pergolesi <matpergo@gmail.com>
2020-03-30 12:50:20 +0200

    Add service annotation to TranslationRulesComposer.java

--- src/main/java/it/cnit/blueprint/composer/rules/TranslationRulesComposer.java
+++ src/main/java/it/cnit/blueprint/composer/rules/TranslationRulesComposer.java
@@ -9,7 +9,9 @@ import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.helpers.MessageFormatter;
+import org.springframework.stereotype.Service;
 
+@Service
 @Slf4j
 @AllArgsConstructor
 public class TranslationRulesComposer {
