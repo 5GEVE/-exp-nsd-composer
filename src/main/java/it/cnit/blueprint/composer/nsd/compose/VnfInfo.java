224584dcf528ec9f34aafea655a8cb030001b11e
Matteo Pergolesi <matpergo@gmail.com>
2020-04-29 12:38:12 +0200

    Refactor VnfInfo to include mgmt and data VirtualLinkConnectivity.

--- src/main/java/it/cnit/blueprint/composer/nsd/compose/VnfInfo.java
+++ src/main/java/it/cnit/blueprint/composer/nsd/compose/VnfInfo.java
@@ -1,16 +1,30 @@
 package it.cnit.blueprint.composer.nsd.compose;
 
+import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.NsVirtualLinkConnectivity;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.VnfProfile;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.VnfToLevelMapping;
-import lombok.AllArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
-public class VnfInfo
-{
+@RequiredArgsConstructor
+public class VnfInfo {
 
-  private String vfndId;
-  private VnfProfile vnfProfile;
-  private VnfToLevelMapping vnfToLevelMapping;
+  private final String vfndId;
+  private final VnfProfile vnfProfile;
+  private final VnfToLevelMapping vnfToLevelMapping;
+  private List<NsVirtualLinkConnectivity> mgmtVlcList = new ArrayList<>();
+  private List<NsVirtualLinkConnectivity> dataVlcList = new ArrayList<>();
+
+  public void setCpdLists(String mgmtVlProfileId) {
+    for (NsVirtualLinkConnectivity vlc : vnfProfile.getNsVirtualLinkConnectivity()) {
+      if (vlc.getVirtualLinkProfileId().equals(mgmtVlProfileId)) {
+        mgmtVlcList.add(vlc);
+      } else {
+        dataVlcList.add(vlc);
+      }
+    }
+  }
 }
