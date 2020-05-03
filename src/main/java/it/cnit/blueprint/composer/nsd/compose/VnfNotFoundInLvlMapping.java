57557273642b2a1969df051841c722087988bc6a
Matteo Pergolesi <matpergo@gmail.com>
2020-05-03 18:22:19 +0200

    Remove unneeded custom exceptions.

--- src/main/java/it/cnit/blueprint/composer/nsd/compose/VnfNotFoundInLvlMapping.java
+++
@@ -1,8 +0,0 @@
-package it.cnit.blueprint.composer.nsd.compose;
-
-public class VnfNotFoundInLvlMapping extends Exception {
-
-  public VnfNotFoundInLvlMapping(String message) {
-    super(message);
-  }
-}
