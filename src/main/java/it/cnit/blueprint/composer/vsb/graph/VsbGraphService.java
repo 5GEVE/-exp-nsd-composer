178b0f60ef452a2b8c0092937a9d1e5e87c0bf75
Matteo Pergolesi <matpergo@gmail.com>
2020-06-12 20:24:16 +0200

    Improve graph export for both NSD and VSB.

--- src/main/java/it/cnit/blueprint/composer/vsb/graph/VsbGraphService.java
+++ src/main/java/it/cnit/blueprint/composer/vsb/graph/VsbGraphService.java
@@ -4,11 +4,20 @@ import it.nextworks.nfvmano.catalogue.blueprint.elements.Blueprint;
 import it.nextworks.nfvmano.catalogue.blueprint.elements.VsComponent;
 import it.nextworks.nfvmano.catalogue.blueprint.elements.VsbEndpoint;
 import it.nextworks.nfvmano.catalogue.blueprint.elements.VsbLink;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import org.jgrapht.Graph;
 import org.jgrapht.alg.connectivity.ConnectivityInspector;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.io.Attribute;
+import org.jgrapht.io.ComponentAttributeProvider;
+import org.jgrapht.io.ComponentNameProvider;
+import org.jgrapht.io.DOTExporter;
+import org.jgrapht.io.DefaultAttribute;
 
 public class VsbGraphService {
 
@@ -64,8 +73,54 @@ public class VsbGraphService {
     return g;
   }
 
-  public String export(Graph<VsbVertex, String> graph) {
-    return "export";
+  public String export(Graph<VsbVertex, String> g) {
+    ComponentNameProvider<VsbVertex> vertexIdProvider = VsbVertex::getVertexId;
+    ComponentNameProvider<VsbVertex> vertexLabelProvider = VsbVertex::toString;
+    ComponentAttributeProvider<VsbVertex> vertexAttributeProvider = v -> {
+      Map<String, Attribute> map = new LinkedHashMap<>();
+      if (v instanceof VsbLinkVertex) {
+        map.put("shape", DefaultAttribute.createAttribute("oval"));
+        map.put("style", DefaultAttribute.createAttribute("filled"));
+        map.put("fillcolor", DefaultAttribute.createAttribute("#dae8fc"));
+        map.put("color", DefaultAttribute.createAttribute("#6c8ebf"));
+      } else if (v instanceof AtomicComponentVertex) {
+        map.put("shape", DefaultAttribute.createAttribute("box"));
+        map.put("style", DefaultAttribute.createAttribute("filled"));
+        map.put("fillcolor", DefaultAttribute.createAttribute("#d5e8d4"));
+        map.put("color", DefaultAttribute.createAttribute("#82b366"));
+      } else if (v instanceof VsbSapVertex) {
+        map.put("shape", DefaultAttribute.createAttribute("circle"));
+        map.put("style", DefaultAttribute.createAttribute("filled"));
+        map.put("fillcolor", DefaultAttribute.createAttribute("#ffe6cc"));
+        map.put("color", DefaultAttribute.createAttribute("#d79b00"));
+        map.put("fixedsize", DefaultAttribute.createAttribute("true"));
+      } else {
+        map = null;
+      }
+      return map;
+    };
+    ComponentNameProvider<String> edgeProvider = new ComponentNameProvider<String>() {
+      @Override
+      public String getName(String component) {
+        if (component.toLowerCase().contains("sap")) {
+          return "";
+        } else {
+          return component;
+        }
+      }
+    };
+    DOTExporter<VsbVertex, String> exporter = new DOTExporter<>(
+        vertexIdProvider,
+        vertexLabelProvider,
+        edgeProvider,
+        vertexAttributeProvider,
+        null);
+    exporter.putGraphAttribute("splines", "false");
+    exporter.putGraphAttribute("overlap", "false");
+    exporter.putGraphAttribute("mindist", "0.5");
+    Writer writer = new StringWriter();
+    exporter.exportGraph(g, writer);
+    return writer.toString();
   }
 
   public boolean isConnected(Graph<VsbVertex, String> g) {
