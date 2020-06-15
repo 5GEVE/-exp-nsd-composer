ef6c1cdf58264686c84f195fcd1d8134e0dbcb35
Matteo Pergolesi <matpergo@gmail.com>
2020-06-15 18:28:11 +0200

    Register classes as spring services.

--- src/main/java/it/cnit/blueprint/composer/vsb/VsbService.java
+++ src/main/java/it/cnit/blueprint/composer/vsb/VsbService.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
 @Slf4j
 @AllArgsConstructor
 public class VsbService {
