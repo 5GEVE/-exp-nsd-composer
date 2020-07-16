6483e5c7d90dd3abb4cbf839619cb7bea204fbb1
Matteo Pergolesi <matpergo@gmail.com>
2020-07-16 16:44:01 +0200

    Remove ServicesController. Start to write NsdController.

--- src/main/java/it/cnit/blueprint/composer/nsd/rest/NsdController.java
+++ src/main/java/it/cnit/blueprint/composer/nsd/rest/NsdController.java
@@ -1,13 +1,11 @@
-package it.cnit.blueprint.composer.rest;
+package it.cnit.blueprint.composer.nsd.rest;
 
 import it.cnit.blueprint.composer.exceptions.NsdGenerationException;
-import it.cnit.blueprint.composer.exceptions.NsdInvalidException;
 import it.cnit.blueprint.composer.nsd.generate.NsdGenerator;
 import it.nextworks.nfvmano.catalogue.blueprint.elements.VsBlueprint;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.Nsd;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,19 +13,17 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @AllArgsConstructor
-public class ServicesController {
+public class NsdController {
 
   private final NsdGenerator nsdGenerator;
 
-  @PostMapping("/services")
+  @PostMapping("/nsd/generate")
   public Nsd generateService(@RequestBody VsBlueprint vsb) {
-    Nsd nsd;
     try {
-      nsd = nsdGenerator.generate(vsb);
+      return nsdGenerator.generate(vsb);
     } catch (NsdGenerationException e) {
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
     }
-    return nsd;
   }
 
 }
---
+++ src/test/java/it/cnit/blueprint/composer/nsd/rest/NsdControllerTest.java
@@ -0,0 +1,77 @@
+package it.cnit.blueprint.composer.nsd.rest;
+
+import static org.junit.Assert.*;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import it.nextworks.nfvmano.catalogue.blueprint.elements.VsBlueprint;
+import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.Nsd;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@Slf4j
+public class NsdControllerTest {
+
+  private ObjectMapper JSON_OM, YAML_OM;
+  static Properties urlProp;
+
+  @Autowired
+  WebApplicationContext webApplicationContext;
+
+  private MockMvc mvc;
+
+  @Before
+  @SneakyThrows
+  public void setup() {
+    // Test Setup
+    urlProp = new Properties();
+    InputStream input = ClassLoader.getSystemResourceAsStream("url.properties");
+    urlProp.load(input);
+    JSON_OM = new ObjectMapper(new JsonFactory()).enable(SerializationFeature.INDENT_OUTPUT);
+    YAML_OM = new ObjectMapper(new YAMLFactory());
+    mvc = MockMvcBuilders
+        .webAppContextSetup(webApplicationContext)
+        .build();
+  }
+
+  @Test
+  @SneakyThrows
+  public void generateService() {
+    // Given
+    VsBlueprint vsb = YAML_OM
+        .readValue(new URL(urlProp.getProperty("vsb_polito_smartcity")), VsBlueprint.class);
+
+    // When
+    MvcResult result = mvc.perform(
+        MockMvcRequestBuilders.post("/nsd/generate")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JSON_OM.writeValueAsString(vsb)))
+        .andReturn();
+
+    // Then
+    assertEquals(200, result.getResponse().getStatus());
+    Nsd actualNsd = JSON_OM.readValue(result.getResponse().getContentAsString(), Nsd.class);
+    Nsd expectedNsd = YAML_OM
+        .readValue(new URL(urlProp.getProperty("vsb_polito_smartcity_nsds")), Nsd.class);
+    assertEquals(YAML_OM.writeValueAsString(expectedNsd), YAML_OM.writeValueAsString(actualNsd));
+  }
+}
