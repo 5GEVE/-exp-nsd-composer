5aabf309cddff581bdcd44c322501cae69572134
Matteo Pergolesi <matpergo@gmail.com>
2020-08-19 16:45:00 +0200

    Add CtxController and tests.

---
+++ src/main/java/it/cnit/blueprint/composer/vsb/rest/CtxController.java
@@ -0,0 +1,69 @@
+package it.cnit.blueprint.composer.vsb.rest;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import it.cnit.blueprint.composer.vsb.graph.VsbGraphService;
+import it.cnit.blueprint.composer.vsb.graph.VsbVertex;
+import it.nextworks.nfvmano.catalogue.blueprint.elements.CtxBlueprint;
+import it.nextworks.nfvmano.catalogue.blueprint.elements.VsBlueprint;
+import it.nextworks.nfvmano.libs.ifa.common.exceptions.MalformattedElementException;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jgrapht.Graph;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+public class CtxController {
+
+  private final VsbGraphService vsbGraphService;
+
+  /**
+   * Validate method. Serialization errors are handled by Spring
+   *
+   * @param ctx object to validate
+   * @return 200 if valid, 400 with validation errors if invalid
+   */
+  @PostMapping("/ctx/validate")
+  public void validate(@RequestBody CtxBlueprint ctx) {
+    try {
+      ctx.isValid();
+    } catch (MalformattedElementException e) {
+      log.debug("Invalid CtxBlueprint: " + e.getMessage());
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+    }
+  }
+
+  @GetMapping("/ctx/schema")
+  public JsonSchema schema() {
+    ObjectMapper J_OBJECT_MAPPER = new ObjectMapper(new JsonFactory())
+        .enable(SerializationFeature.INDENT_OUTPUT);
+    try {
+      return new JsonSchemaGenerator(J_OBJECT_MAPPER).generateSchema(CtxBlueprint.class);
+    } catch (JsonMappingException e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+    }
+  }
+
+  @PostMapping("/ctx/graph")
+  public Map<String, String> graph(@RequestBody CtxBlueprint ctx) {
+    Graph<VsbVertex, String> graph = vsbGraphService.buildGraph(ctx);
+    return new HashMap<String, String>() {
+      {
+        put("graph", vsbGraphService.export(graph));
+      }
+    };
+  }
+}
