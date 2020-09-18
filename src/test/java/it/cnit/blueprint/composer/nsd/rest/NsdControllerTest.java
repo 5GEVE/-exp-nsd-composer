3d2bd7b3e1cd6023b4c49776d5e4237bef36fddc
Matteo Pergolesi <matpergo@gmail.com>
2020-09-18 13:45:04 +0200

    Remove translation rules from NsdController. Fix tests.

--- src/test/java/it/cnit/blueprint/composer/nsd/rest/NsdControllerTest.java
+++ src/test/java/it/cnit/blueprint/composer/nsd/rest/NsdControllerTest.java
@@ -3,21 +3,18 @@ package it.cnit.blueprint.composer.nsd.rest;
 import static org.junit.Assert.assertEquals;
 
 import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.module.jsonSchema.types.ObjectSchema;
 import it.nextworks.nfvmano.catalogue.blueprint.elements.CtxBlueprint;
 import it.nextworks.nfvmano.catalogue.blueprint.elements.VsBlueprint;
-import it.nextworks.nfvmano.catalogue.blueprint.elements.VsdNsdTranslationRule;
 import it.nextworks.nfvmano.catalogue.blueprint.messages.OnBoardVsBlueprintRequest;
 import it.nextworks.nfvmano.catalogue.blueprint.messages.OnboardCtxBlueprintRequest;
 import it.nextworks.nfvmano.libs.ifa.descriptors.nsd.Nsd;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -135,23 +132,15 @@ public class NsdControllerTest {
         .readValue(new URL(urlProp.getProperty("vsb_ares2t_tracker")), VsBlueprint.class);
     Nsd vsbNsd = YAML_OM
         .readValue(new URL(urlProp.getProperty("vsb_ares2t_tracker_nsds")), Nsd.class);
-    List<VsdNsdTranslationRule> vsbTr = YAML_OM
-        .readValue(new URL(urlProp.getProperty("vsb_ares2t_tracker_tr")),
-            new TypeReference<List<VsdNsdTranslationRule>>() {
-            });
     OnBoardVsBlueprintRequest vsbRequest = new OnBoardVsBlueprintRequest(vsb,
-        Collections.singletonList(vsbNsd), vsbTr);
+        Collections.singletonList(vsbNsd), null);
 
     CtxBlueprint ctxb = YAML_OM
         .readValue(new URL(urlProp.getProperty("ctx_delay")), CtxBlueprint.class);
     Nsd ctxbNsd = YAML_OM
         .readValue(new URL(urlProp.getProperty("ctx_delay_nsds")), Nsd.class);
-    List<VsdNsdTranslationRule> ctxbTr = YAML_OM
-        .readValue(new URL(urlProp.getProperty("ctx_delay_tr")),
-            new TypeReference<List<VsdNsdTranslationRule>>() {
-            });
     OnboardCtxBlueprintRequest ctxbRequest = new OnboardCtxBlueprintRequest(ctxb,
-        Collections.singletonList(ctxbNsd), ctxbTr);
+        Collections.singletonList(ctxbNsd), null);
     Context c = new Context(ctxbRequest, null);
 
     ComposeRequest request = new ComposeRequest(vsbRequest, new Context[]{c});
@@ -177,12 +166,8 @@ public class NsdControllerTest {
         .readValue(new URL(urlProp.getProperty("ctx_delay")), CtxBlueprint.class);
     Nsd delayNsd = YAML_OM
         .readValue(new URL(urlProp.getProperty("ctx_delay_nsds")), Nsd.class);
-    List<VsdNsdTranslationRule> delayTr = YAML_OM
-        .readValue(new URL(urlProp.getProperty("ctx_delay_tr")),
-            new TypeReference<List<VsdNsdTranslationRule>>() {
-            });
     OnboardCtxBlueprintRequest delayRequest = new OnboardCtxBlueprintRequest(delayCtxB,
-        Collections.singletonList(delayNsd), delayTr);
+        Collections.singletonList(delayNsd), null);
     Context delay = new Context(delayRequest, null);
 
     CtxBlueprint trafficCtxB = YAML_OM
@@ -214,17 +199,13 @@ public class NsdControllerTest {
 
     // Then
     assertEquals(200, result.getResponse().getStatus());
-    ComposeResponse response = JSON_OM
-        .readValue(result.getResponse().getContentAsString(), ComposeResponse.class);
-    log.info("Response body:\n{}", JSON_OM.writeValueAsString(response));
-    Nsd actualNsd = response.getExpNsd();
+    Nsd actualNsd = JSON_OM .readValue(result.getResponse().getContentAsString(),Nsd.class);
     actualNsd.setNsdIdentifier("58886b95-cd29-4b7b-aca0-e884caaa5c68");
     actualNsd.setNsdInvariantId("ae66294b-8dae-406c-af70-f8516e310965");
     InputStream in = getClass().getResourceAsStream(
         "/expb_ares2t_tracker_delay_nsds_passthrough.yaml");
     Nsd expectedNsd = YAML_OM.readValue(in, Nsd[].class)[0];
-    assertEquals(YAML_OM.writeValueAsString(expectedNsd),
-        YAML_OM.writeValueAsString(response.getExpNsd()));
+    assertEquals(YAML_OM.writeValueAsString(expectedNsd), YAML_OM.writeValueAsString(actualNsd));
   }
 
   @Test
@@ -242,16 +223,12 @@ public class NsdControllerTest {
 
     // Then
     assertEquals(200, result.getResponse().getStatus());
-    ComposeResponse response = JSON_OM
-        .readValue(result.getResponse().getContentAsString(), ComposeResponse.class);
-    log.info("Response body:\n{}", JSON_OM.writeValueAsString(response));
-    Nsd actualNsd = response.getExpNsd();
+    Nsd actualNsd = JSON_OM.readValue(result.getResponse().getContentAsString(), Nsd.class);
     actualNsd.setNsdIdentifier("0176eb11-f613-4b40-bd71-c7a4ea4f0896");
     actualNsd.setNsdInvariantId("71544f81-653a-4dc1-a49c-a34bc1e17976");
     InputStream in = getClass().getResourceAsStream("/expb_polito_smartcity_nsd.yaml");
     Nsd expectedNsd = YAML_OM.readValue(in, Nsd.class);
-    assertEquals(YAML_OM.writeValueAsString(expectedNsd),
-        YAML_OM.writeValueAsString(response.getExpNsd()));
+    assertEquals(YAML_OM.writeValueAsString(expectedNsd), YAML_OM.writeValueAsString(actualNsd));
   }
 
   @Test
