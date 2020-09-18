8520416e3ff8c07a714ac1906312ad7075e84085
Matteo Pergolesi <matpergo@gmail.com>
2020-09-18 17:06:23 +0200

    Fix openApi version

--- src/main/java/it/cnit/blueprint/composer/OpenApiConfiguration.java
+++ src/main/java/it/cnit/blueprint/composer/OpenApiConfiguration.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.info.License;
         description =
             "A REST API module to compose experiment NSDs. It can also validate blueprints and"
                 + " generate NSDs from them.",
-        version = "v0.0.4",
+        version = "v0.1.0",
         license = @License(name = "Apache 2.0")
     )
 )
