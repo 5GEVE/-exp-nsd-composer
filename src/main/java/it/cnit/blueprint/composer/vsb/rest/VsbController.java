f6d5e9f57ade66788ace0fe5ed9df4ccc144f409
Matteo Pergolesi <matpergo@gmail.com>
2020-09-29 12:10:36 +0200

    Improve OpenAPI for VsbController

--- src/main/java/it/cnit/blueprint/composer/vsb/rest/VsbController.java
+++ src/main/java/it/cnit/blueprint/composer/vsb/rest/VsbController.java
@@ -6,6 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import it.cnit.blueprint.composer.commons.ZipService;
 import it.cnit.blueprint.composer.vsb.VsbService;
 import it.cnit.blueprint.composer.vsb.graph.VsbGraphService;
@@ -22,7 +26,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jgrapht.Graph;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +49,7 @@ public class VsbController {
   private final ZipService zipService;
 
   @PostMapping("/addMgmt")
+  @Operation(description = "Adds a management connectivity service to a VSB if not present")
   public VsBlueprint addMgmtConnService(@RequestBody @Valid VsBlueprint vsb) {
     validate(vsb);
     if (vsb.getConnectivityServices().stream().noneMatch(VsbLink::isManagement)) {
@@ -59,6 +66,7 @@ public class VsbController {
    * @return 200 if valid, 400 with validation errors if invalid
    */
   @PostMapping("/validate")
+  @Operation(description = "Validates a VSB")
   public void validate(@RequestBody @Valid VsBlueprint vsb) {
     try {
       vsb.isValid();
@@ -69,6 +77,7 @@ public class VsbController {
   }
 
   @GetMapping("/schema")
+  @Operation(description = "Generates the JSON Schema for a VSB")
   public JsonSchema schema() {
     ObjectMapper J_OBJECT_MAPPER = new ObjectMapper(new JsonFactory())
         .enable(SerializationFeature.INDENT_OUTPUT);
@@ -80,6 +89,12 @@ public class VsbController {
   }
 
   @PostMapping("/graph")
+  @Operation(description = "Generates PNG images to visualize the topology of a VSB",
+      responses = @ApiResponse(
+      responseCode = "200",
+      description = "A zip file containing the images (PNG) representing the VSB in input",
+      content = @Content(mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE),
+      headers = @Header(name = HttpHeaders.CONTENT_DISPOSITION)))
   public ResponseEntity<InputStreamResource> graph(@RequestBody @Valid VsBlueprint vsb) {
     validate(vsb);
     File tempFile;
