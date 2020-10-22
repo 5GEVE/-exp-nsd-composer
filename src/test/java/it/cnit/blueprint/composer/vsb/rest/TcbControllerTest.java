4af20473b232f14e005ff988ed2739abd48b0aa7
Matteo Pergolesi <matpergo@gmail.com>
2020-10-22 18:09:13 +0200

    Add tests for TcbControllerTest.

--- src/test/java/it/cnit/blueprint/composer/vsb/rest/TcbControllerTest.java
+++ src/test/java/it/cnit/blueprint/composer/vsb/rest/TcbControllerTest.java
@@ -1,13 +1,15 @@
 package it.cnit.blueprint.composer.vsb.rest;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.module.jsonSchema.types.ObjectSchema;
-import it.nextworks.nfvmano.catalogue.blueprint.elements.TestCaseBlueprint;
+import it.nextworks.nfvmano.catalogue.blueprint.messages.OnboardTestCaseBlueprintRequest;
 import java.io.InputStream;
 import java.util.Properties;
 import lombok.SneakyThrows;
@@ -54,10 +56,11 @@ public class TcbControllerTest {
 
   @Test
   @SneakyThrows
-  public void validate() {
+  public void validate200() {
     // Given
-    InputStream in = getClass().getResourceAsStream("/TCB_Ares2T_Service_v5-RC-v2.yaml");
-    TestCaseBlueprint tcb = YAML_OM.readValue(in, TestCaseBlueprint.class);
+    InputStream in = getClass().getResourceAsStream("/tcb_cnit_smart_city_1.yaml");
+    OnboardTestCaseBlueprintRequest tcb = YAML_OM
+        .readValue(in, OnboardTestCaseBlueprintRequest.class);
 
     // When
     MvcResult result = mvc.perform(
@@ -67,7 +70,32 @@ public class TcbControllerTest {
         .andReturn();
 
     // Then
+    if (result.getResponse().getErrorMessage() != null) {
+      log.error(result.getResponse().getErrorMessage());
+    }
     assertEquals(200, result.getResponse().getStatus());
+  }
+
+  @Test
+  @SneakyThrows
+  public void validate400() {
+    // Given
+    InputStream in = getClass().getResourceAsStream("/tcb_cnit_smart_city_1_param_error.yaml");
+    OnboardTestCaseBlueprintRequest tcb = YAML_OM
+        .readValue(in, OnboardTestCaseBlueprintRequest.class);
+
+    // When
+    MvcResult result = mvc.perform(
+        MockMvcRequestBuilders.post("/tcb/validate")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JSON_OM.writeValueAsString(tcb)))
+        .andReturn();
+
+    // Then
+    log.info(result.getResponse().getErrorMessage());
+    assertThat(result.getResponse().getErrorMessage(),
+        containsString("Parameter '$$pippo' in configurationScript is not declared"));
+    assertEquals(400, result.getResponse().getStatus());
   }
 
   @Test
